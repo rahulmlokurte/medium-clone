b6cd4c9989974c497e6018c5fb5dcc984dafe859
rahullokurte <rahul.m.lokurte@gmail.com>
2022-08-11 18:49:13 +0530

    Adding sanity config files

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,13 @@ const Home: NextPage = () => {
         <div className='px-10 space-y-5'>
           <h1 className='text-6xl max-w-xl font-serif'>
             <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read and connect</h1>
-          <h2>Its easy and free to post your thinking on any topic and connect
+          <h2>It's easy and free to post your thinking on any topic and connect
             with millions of readers</h2></div>
         <img className='hidden md:inline-flex h-32 lg:h-full'
           src='https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png' alt='' />
       </div>
+
+      {/* Posts */}
     </div>
   )
 }
