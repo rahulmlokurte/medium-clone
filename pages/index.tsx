d3f9b577052502d1b9129ded016d3a8001821bbf
rahullokurte <rahul.m.lokurte@gmail.com>
2022-08-16 17:06:19 +0530

    getstaticpaths

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,13 +35,13 @@ const Home = ({ posts }: Props) => {
        gap-3 md:gap-6 p-2 md:p-6'>
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className='group cursor-pointer'>
+            <div className='border rounded-lg group cursor-pointer overflow-hidden'>
               <img className='h-60 w-full object-cover group-hover:scale-105
               transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt='' />
               <div className='flex justify-between p-5 bg-white'>
                 <div>
-                  <p>{post.title}</p>
-                  <p>{post.description} by {post.author.name}</p>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
                 </div>
                 <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()!} alt='' />
               </div>
